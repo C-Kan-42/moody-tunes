780a8a829c837157fb2b009d44d7c1f0f41ac3e2
Prescott <carlsonprescott@gmail.com>
2020-01-22 11:52:35 -0800

    user-auth now fixed and working for backend

--- validation/register.js
+++ validation/register.js
@@ -2,47 +2,51 @@ const Validator = require("validator");
 const validText = require("./valid-text");
 
 module.exports = function validateRegisterInput(data) {
-    let errors = {};
+  let errors = {};
 
-    data.username = validText(data.username) ? data.username : "";
-    data.email = validText(data.email) ? data.email : "";
-    data.password = validText(data.password) ? data.password : "";
-    data.password2 = validText(data.password2) ? data.password2 : "";
+  data.username = validText(data.username) ? data.username : "";
+  data.email = validText(data.email) ? data.email : "";
+  data.password = validText(data.password) ? data.password : "";
+  data.password2 = validText(data.password2) ? data.password2 : "";
 
-    if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
-        errors.username = "Username must be between 2 and 30 characters";
-    }
+  if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
+    errors.username = "Username must be between 2 and 30 characters";
+  }
 
-    if (Validator.isEmpty(data.username)) {
-        errors.username = "Username field is required";
-    }
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Username field is required";
+  }
 
-    if (Validator.isEmpty(data.email)) {
-        errors.email = "Email field is required";
-    }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
 
-    if (!Validator.isEmail(data.email)) {
-        errors.email = "Email is invalid";
-    }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
 
-    if (Validator.isEmpty(data.password)) {
-        errors.password = "Password field is required";
-    }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password is required";
+  }
 
-    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-        errors.password = "Password must be at least 6 characters";
-    }
+  if (!Validator.isLength(data.password, { min: 2, max: 30 })) {
+    errors.password = "Password must be between 2 and 30 characters";
+  }
 
-    if (Validator.isEmpty(data.password2)) {
-        errors.password2 = "Confirm Password field is required";
-    }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Password is required";
+  }
 
-    if (!Validator.equals(data.password, data.password2)) {
-        errors.password2 = "Passwords must match";
-    }
+  if (!Validator.isLength(data.password2, { min: 2, max: 30 })) {
+    errors.password2 = "Password must be between 2 and 30 characters";
+  }
 
-    return {
-        errors,
-        isValid: Object.keys(errors).length === 0
-    };
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
 };

647d9c89ab0b47ea968f50b844e160e7a559f944
mayanag <mayanag@ucla.edu>
2020-01-23 15:45:34 -0800

    add main page styling

--- frontend/src/components/app/app.js
+++ frontend/src/components/app/app.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { AuthRoute, ProtectedRoute } from "../../util/route_util";
 import { Switch } from "react-router-dom";
-import NavBarContainer from "./nav/navbar_container";
-import Modal from "./modal/modal";
+import NavBarContainer from "../nav/navbar_container";
+import Modal from "../modal/modal";
+import './app.css';
 
-import MainPage from "./main/main_page";
+
+import MainPage from "../main/main_page";
 // import LoginFormContainer from "./session/login_form_container";
 // import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
-    <div>
+    <div className="app">
         <Modal/>
         <NavBarContainer />
         <Switch>

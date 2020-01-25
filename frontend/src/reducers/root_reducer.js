8f148686f23953ad0d3891548d6024f88c45c20b
Kevin <knguyen3415@gmail.com>
2020-01-24 16:35:52 -0800

    Setup reaction components

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from "redux";
 import session from "./session_reducer";
 import errors from './errors_reducer';
 import ui from './ui_reducer';
+import playlists from './playlists_reducer';
+import reacitons from './reactions_reducer';
 
 const RootReducer = combineReducers({
     session,
     errors,
-    ui
+    ui,
+    playlists,
+    reactions,
 });
 
 export default RootReducer;

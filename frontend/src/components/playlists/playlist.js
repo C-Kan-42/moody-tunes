40075ee44f3923c0227fdde407e78206001f44b4
Carina Kan <carina.d.kan@gmail.com>
2020-02-17 10:23:20 -0800

    Attempt to render playlist show page

--- frontend/src/components/playlists/playlist.js
+++ frontend/src/components/playlists/playlist.js
@@ -23,6 +23,7 @@ class Playlist extends React.Component {
     return(
       <li className="playlist-item" >
         <Link to={`/playlists/${playlist.id}`}>
+        {/* check HOW TO REFERENCE mongo ids */}
           <h2>{playlist.title}</h2>
           {/* <ul className="song-list">
             {playlist.songs.map(song => 

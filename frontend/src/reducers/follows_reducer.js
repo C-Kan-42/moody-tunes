17fabb291837e120dd9205d2b41dad9ed3ca8c10
Kevin <knguyen3415@gmail.com>
2020-02-20 11:45:55 -0800

    Add follows reducer

---
+++ frontend/src/reducers/follows_reducer.js
@@ -0,0 +1,23 @@
+import {
+    RECEIVE_FOLLOWS,
+    RECEIVE_USER_FOLLOWS
+} from '../actions/follow_actions';
+
+const FollowsReducer = (state = { all: {}, user: {}}, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+
+    switch(action.type) {
+        case RECEIVE_FOLLOWS:
+            newState.all = action.follows.data;
+            return newState;
+
+        case RECEIVE_USER_FOLLOWS:
+            newState.user = action.follows.data;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default FollowsReducer;

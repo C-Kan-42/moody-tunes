b8c7d3346353562b17a0b316fbda89e01b777767
Carina Kan <carina.d.kan@gmail.com>
2020-03-02 22:15:29 -0800

    Fix session error reducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import playlists from './playlists_reducer';
 import reactions from './reactions_reducer';
 import users from './users_reducer';
 import follows from './follows_reducer';
+import errors from './errors_reducer';
 
 const entitiesReducer = combineReducers({
   playlists,
   reactions,
   users,
-  follows
+  follows,
+  errors
 });
 
 export default entitiesReducer;
